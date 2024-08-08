cd3f3f404d8204f734f0e741810fc2ce9394bf69
Sachvk <serg22011985@gmail.com>
2024-08-08 15:32:02 +0300

    Add files via upload

---
+++ module_add.py
@@ -0,0 +1,9 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = list(sorted(students))
+grades = sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])
+print(grades)
+result = {}
+for i in range(0, len(students)):
+    result[students[i]] = grades[i]
+print(result)
